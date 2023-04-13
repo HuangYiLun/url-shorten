d761283f33f00806e8faa5ec886706e3cbfc8ac8
HuangYiLun <wk5018102@gmail.com>
2023-04-13 14:28:08 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express')
-const app = express()
+const exphbs = require('express-handlebars')
 
+const app = express()
 const port = 3000
 
+app.engine('handlebars', exphbs.engine({ defaultLayout: "main" }))
+app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
-  res.send('my first shortener project')
+  res.render('index')
+})
+
+app.get('/shorten', (req, res) => {
+  res.render('shorten')
 })
 
 
