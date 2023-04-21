5d57e8e2010031dd73875cb27b321f9b3e67fc27
HuangYiLun <wk5018102@gmail.com>
2023-04-21 15:08:55 +0800

    feat: modify shortenURL.js

--- shortenURL.js
+++ shortenURL.js
@@ -3,15 +3,11 @@
 function shortenURL(options) {
   // 英文數字表
   const BASE_62_CHAR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-  // 英文數字表最大index
-  const MAX = 61
-  // 英文數字表最小index
-  const MIN = 0
 
   let result = ""
 
   for (let i = 1; i <= options; i++) {
-    const randomIndex = Math.floor(Math.random() * (MAX - MIN + 1) + MIN)
+    const randomIndex = Math.floor(Math.random() * BASE_62_CHAR.length)
     result += BASE_62_CHAR[randomIndex]
   }
   return result
